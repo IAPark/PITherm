d50556fe5d074edffe54ba7c53d3a192269e2c12
Isaac Park <isaac.a.park@gmail.com>
2015-08-08 15:55:53 -0400

    Adds cleanup

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
+# assumes we only do this from one program
+GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 
-while True:
-    GPIO.output(4, False)
-    print("off")
-    time.sleep(2)
-    GPIO.output(4, True)
-    print("on")
+try:
+    while True:
+        GPIO.output(4, False)
+        print("off")
+        time.sleep(2)
+        GPIO.output(4, True)
+        print("on")
+except:
+    GPIO.cleanup()
 
 
