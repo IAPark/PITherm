24eb5a799a5de1cefbbbb58a254ef6157bcbea14
Isaac Park <isaac.a.park@gmail.com>
2015-08-08 15:51:18 -0400

    Adds a Simple Demo

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
-print("this is just the start")
-print("this is one too")
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+
+while True:
+    GPIO.output(4, False)
+    time.sleep(2)
+    GPIO.output(4, True)
+
