8d100a2e56ce6554b3e1461130d62bff6499271a
Isaac Park <isaac.a.park@gmail.com>
2015-08-08 16:19:11 -0400

    Adds a Way to Stop
    Applying high to pin 27 should now stop the loop

--- run.py
+++ run.py
@@ -3,16 +3,17 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
+GPIO.setup(27, GPIO.INPUT)
 
 try:
-    while True:
+    while not GPIO.input(27):
         GPIO.output(4, False)
         print("off")
         time.sleep(2)
         GPIO.output(4, True)
         print("on")
         time.sleep(2)
-except:
+finally:
     GPIO.cleanup()
 
 
