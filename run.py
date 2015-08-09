ff8b8a4abd6f6a0245cd19a3c7e84a2b48e15799
Isaac Park <isaac.a.park@gmail.com>
2015-08-08 21:56:15 -0400

    Adds Temp Monitoring

--- run.py
+++ run.py
@@ -1,20 +1,30 @@
 from flask import Flask
 app = Flask(__name__)
 
-import threading
 import RPi.GPIO as GPIO
-import time
+import smbus
 
 on = False
+bus = smbus.SMBus(1)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(27, GPIO.IN)
 
+def read_temp(bus):
+    return "temp is:" + str(bus.read_byte(0x48))
+
+
 @app.route('/')
-def hello_world():
+def toggle_relay():
     global on
     on = not on
     GPIO.output(4, on)
+    return "On" if on else "Off"
+
+@app.route('/temp')
+def toggle_temp():
+    global on
     return "On" if on else "Off"
 
 app.run(host='0.0.0.0')
