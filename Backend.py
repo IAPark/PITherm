3cc53454892f52cdd02b1c1e46b9899610864d51
Isaac Park <isaac.a.park@gmail.com>
2015-08-12 20:58:13 -0400

    Implements the Start of the Backend

---
+++ Backend.py
@@ -0,0 +1,42 @@
+__author__ = 'Isaac'
+from services import Services
+from services.temp_monitor import TempMonitor
+from services.air_handler import AirHandler
+from services.database import DB
+from hardware_abstraction import Pin, TemperatureSensor
+
+class FakePin:
+
+    def __init__(self, name):
+        self.name = name
+        self.state = False
+
+    def sense(self):
+        return False
+
+    def set(self, state):
+        self.state = state
+
+
+    def get(self):
+        return self.state
+
+Services.TempMonitor = TempMonitor(TemperatureSensor(0x48))
+Services.DB = DB()
+Services.AirHandler = AirHandler(AC_pin=Pin(4), fan_pin=FakePin("fan"), heater_pin=FakePin("heater"), db=Services.DB)
+import time
+
+from Services.thermostat import Thermostat
+Services.Thermostat = Thermostat(Services.AirHandler, 3)
+
+
+def main_loop():
+    Services.TempMonitor.check_temp()
+    time.sleep(30)
+
+@Services.TempMonitor.temp_changed
+def temp_change(temp: int):
+    print("temp now: " + str(temp))
+
+while True:
+    main_loop()
---
+++ hardware_abstraction/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'Isaac'
+
+from hardware_abstraction.pin import Pin
+from hardware_abstraction.temperature_sensor import TemperatureSensor
