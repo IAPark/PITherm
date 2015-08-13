7826c8cb74daae0dc6477560987d02de86e06aed
Isaac Park <isaac.a.park@gmail.com>
2015-08-12 21:57:09 -0400

    Uses Floats

--- Backend.py
+++ Backend.py
@@ -34,7 +34,7 @@ Services.Thermostat.set_heater_target(0)
 
 
 def centigrade_to_fahrenheit(c):
-    return c * 212/100 + 32
+    return c * 212.0/100.0 + 32
 
 def main_loop():
     Services.TempMonitor.check_temp()
