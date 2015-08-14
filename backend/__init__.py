325431ed55b32799a2aaf61472bfc9388921f92a
Isaac Park <isaac.a.park@gmail.com>
2015-08-13 21:40:09 -0400

    Fixes More Imports
    Broken in the refactoring

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,19 +3,20 @@ from time import sleep
 from backend.router import Router
 from ticker import default_ticker
 import services
-from services import TempMonitor
+from services.temp_monitor import TempMonitor
 from hardware_abstraction import TemperatureSensor, Pin
 
 services.TempMonitor = TempMonitor(TemperatureSensor(0x48))
-from services import DB
+from services.database import DB
 services.DB = DB()
-from services import AirHandler
+from services.air_handler import AirHandler
 from debugging import FakePin
 services.AirHandler = AirHandler(AC_pin=Pin(4), fan_pin=FakePin("fan"), heater_pin=FakePin("heater"), db=services.DB)
 
 router = Router()
 import debugging
 from API_backend import general
+
 
 def start(command_queue, response_queue):
     router.command_queue = command_queue
