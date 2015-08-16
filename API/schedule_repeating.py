4ce2086d066a6088a8a0da88999bd1b2b468219a
Isaac Park <isaac.a.park@gmail.com>
2015-08-16 01:03:21 -0400

    The Problem Wasn't in the Code I was Passing Bad JSON

--- API/schedule_repeating.py
+++ API/schedule_repeating.py
@@ -10,29 +10,21 @@ api = Blueprint("schedule_repeating", __name__, url_prefix='/schedule/repeating'
 # Routes to modify the non repeating schedule
 @api.route('/', methods=["GET"])
 @logged_in_route
-def get_schedule_repeating():
+def get_schedule():
     result = StateChangeRepeating.get_all_dic()
     return Response(json.dumps({"data": result}, default=json_util.default), mimetype='application/json')
 
 
 @api.route('/', methods=["POST"])
 @logged_in_route
-def add_schedule_repeating():
-    print("in function")
-    print(request.mimetype)
-    data = json.loads(request.data)
-    print(json.loads(request.data))
-    print(data)
-    result = StateChangeRepeating.from_dictionary(data)
-    print(result)
-    result = result.save()
-    print(result)
+def add_schedule():
+    result = StateChangeRepeating.from_dictionary(request.get_json(force=True)).save()
     return Response(json.dumps({"data": result}, default=json_util.default), mimetype='application/json')
 
 
 @api.route('/', methods=["DELETE"])
 @logged_in_route
-def remove_schedule_repeating():
+def remove_schedule():
     to_remove = StateChangeRepeating.from_dictionary(request.get_json(force=True))
     result = schedule.remove({"_id": to_remove.id})
     return Response(json.dumps({"data": result}, default=json_util.default), mimetype='application/json')
