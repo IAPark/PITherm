889b88de0bcb233af24d17723b02c2555bc8357b
Isaac Park <isaac.a.park@gmail.com>
2015-08-20 22:17:55 -0400

    Revert "Debugging"
    This reverts commit e9e3cdc8077a558aed39120854e7c0df2b1625fc.

--- API/user.py
+++ API/user.py
@@ -3,12 +3,12 @@ from security import logged_in_route
 from bson import json_util
 import json
 from flask.ext.cors import CORS
-api = Blueprint("user", __name__, url_prefix='/user/')
+api = Blueprint("user", __name__, url_prefix='/user')
 CORS(api)
 
 
 # Login
-@api.route('/login')
+@api.route('/')
 @logged_in_route
 def login():
     return Response(json.dumps({"data": "logged in"}, default=json_util.default), mimetype='application/json')
