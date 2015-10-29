8a5418253a287d881c9c6080d757ce38b50c9714
Isaac Park <isaac.a.park@gmail.com>
2015-10-28 23:55:28 -0400

    Boiler Plate For Database Added
    Trying to finish this up. Will probably add routes similar to
    state_change_weekly and maybe some to the root to handle getting the
    current state that should be used and to register a listener

---
+++ src/services/Database/state_changes/controller.py
@@ -0,0 +1,31 @@
+import json
+from bson import json_util
+from flask import Blueprint, Response, request
+from flask.ext.cors import CORS
+from model import StateChange
+import model
+
+api = Blueprint("state_changes", __name__, url_prefix='/state_changes')
+CORS(api)
+
+
+@api.route('/', methods=["GET"])
+def get_state_changes():
+    result = StateChange.get_all_dic()
+    return Response(json.dumps({"data": result}, default=json_util.default), mimetype='application/json')
+
+@api.route('/current')
+def get_current_state_change():
+    return "not implemented"
+
+
+@api.route('/', methods=["post"])
+def add_state_changes():
+    result = StateChange.from_dictionary(request.get_json(force=True)).save()
+    return Response(json.dumps({"data": result}, default=json_util.default), mimetype='application/json')
+
+@api.route('/', methods=["delete"])
+def delete_state_changes():
+    to_remove = StateChange.from_dictionary(request.get_json(force=True))
+    result = model.remove(to_remove.id)
+    return Response(json.dumps({"data": result}, default=json_util.default), mimetype='application/json')
