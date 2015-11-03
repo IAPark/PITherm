292c5d25e1fd5852f61171c761499cee92e0db27
Isaac Park <isaac.a.park@gmail.com>
2015-11-02 19:14:35 -0500

    Register state_changes_weekly API

--- src/services/Database/run.py
+++ src/services/Database/run.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from state_changes import api
+import state_changes
+import state_changes_weekly
+
 app = Flask(__name__)
 
-app.register_blueprint(api)
+app.register_blueprint(state_changes.api)
+app.register_blueprint(state_changes_weekly.api)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
