586bf36ab9a5439b0d4fc1ea6f608b98de5a2a27
Isaac Park <isaac.a.park@gmail.com>
2015-11-07 13:07:44 -0500

    Add Command line Arg to Change Port for Schedule

--- src/services/Schedule/Schedule.py
+++ src/services/Schedule/Schedule.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 from datetime import datetime
 
@@ -7,6 +8,10 @@ from src import state_changes_weekly
 
 from src.state_changes import StateChange
 from src.state_changes_weekly import StateChangeWeekly
+
+parser = argparse.ArgumentParser(description='Record a Schedule of States both by exact time and by time of the week')
+parser.add_argument("-p", "--port", type=int, help="the port to run server on", default=5003)
+args = parser.parse_args()
 
 app = Flask(__name__)
 
@@ -25,4 +30,4 @@ def get_state(time: str):
     return Response(json.dumps({"data": None, "errors": ["can't find any scheduled states"]}))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=True, port=args.port)
