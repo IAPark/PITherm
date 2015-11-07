72f47900c5242564f97285d7cc910a53caaff9a1
Isaac Park <isaac.a.park@gmail.com>
2015-11-06 20:29:33 -0500

    Add State Route
    Fetch the Currently Active State

--- src/services/Schedule/Schedule.py
+++ src/services/Schedule/Schedule.py
@@ -1,12 +1,28 @@
-from flask import Flask
+import json
+from datetime import datetime
+
+from flask import Flask, Response
 from src import state_changes
 from src import state_changes_weekly
+
+from src.state_changes import StateChange
+from src.state_changes_weekly import StateChangeWeekly
 
 app = Flask(__name__)
 
 app.register_blueprint(state_changes.api)
 app.register_blueprint(state_changes_weekly.api)
 
+@app.route("/state/<time>")
+def get_state(time: str):
+    scheduled = StateChange.get_current(datetime.utcfromtimestamp(int(time)))
+    if scheduled is not None:
+        return Response(json.dumps({"data": scheduled.to_dictionary()["state"]}))
+
+    repeating = StateChangeWeekly.get_current(datetime.utcfromtimestamp(int(time)))
+    if repeating is not None:
+        return Response(json.dumps({"data": repeating.to_dictionary()["state"]}))
+    return Response(json.dumps({"data": None, "errors": ["can't find any scheduled states"]}))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5003)
--- src/services/Schedule/src/state_changes_weekly/__init__.py
+++ src/services/Schedule/src/state_changes_weekly/__init__.py
@@ -1,1 +1,2 @@
 from .controller import api
+from .model import StateChangeWeekly
