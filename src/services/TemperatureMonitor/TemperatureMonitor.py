628d777e3751ec8e38f1b98f558799b28cda1569
Isaac Park <isaac.a.park@gmail.com>
2015-12-13 00:27:15 -0500

    Allow Control of Interval and Observers

--- src/services/TemperatureMonitor/TemperatureMonitor.py
+++ src/services/TemperatureMonitor/TemperatureMonitor.py
@@ -3,7 +3,20 @@ import sys
 from src.TemperatureMonitor import TemperatureMonitor
 from src.temperature import TemperatureSensor
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Broadcast temperatures to URLs')
+parser.add_argument('observers', metavar='N', type=str, nargs='+',
+                   help='the observers', default=())
+parser.add_argument("-i", "--interval", type=int, help="the period between testing the temperature", default=60)
+parser.add_argument("-s", "--smoothing", type=int, help="the number of samples to average when broadcasting a result", default=60)
+
+args = parser.parse_args()
+
 SENSOR_ADDRESS = 0x48
 
-tempMonitor = TemperatureMonitor(TemperatureSensor(SENSOR_ADDRESS), observers=sys.argv[1:])
+tempMonitor = TemperatureMonitor(TemperatureSensor(SENSOR_ADDRESS),
+                                 interval=args.interval,
+                                 smoothing=args.smoothing,
+                                 observers=args.observers)
 tempMonitor.run()
